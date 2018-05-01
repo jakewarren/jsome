c16f1be6b6e0e82ef5d92fc4c8eca1755f64aa70
Jake Warren <jakewarren@users.noreply.github.com>
2018-05-01 10:29:34 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# jsome
+[![Build Status](https://travis-ci.org/jakewarren/jsome.svg?branch=master)](https://travis-ci.org/jakewarren/jsome/)
+[![GitHub release](http://img.shields.io/github/release/jakewarren/jsome.svg?style=flat-square)](https://github.com/jakewarren/jsome/releases])
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)](https://github.com/jakewarren/jsome/blob/master/LICENSE)
+[![Go Report Card](https://goreportcard.com/badge/github.com/jakewarren/jsome)](https://goreportcard.com/report/github.com/jakewarren/jsome)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=shields)](http://makeapullrequest.com)
+> cli utility for colorizing your JSON
+
+
+## Install
+### Option 1: Binary
+
+Download the latest release from [https://github.com/jakewarren/jsome/releases/latest](https://github.com/jakewarren/jsome/releases/latest)
+
+### Option 2: From source
+
+```
+go get github.com/jakewarren/jsome
+```
+
+## Screenshot
+
+![](screenshot.png)
+
+## Usage
+
+## Acknowledgements
+
+[Javascipt/Jsome](https://github.com/Javascipt/Jsome) - for inspiration of the tool, and for the name I shamelessly copied  
+[nwidger/jsoncolor](https://github.com/nwidger/jsoncolor) - awesome library that powers this tool
+
+## Changes
+
+All notable changes to this project will be documented in the [changelog].
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## License
+
+MIT © 2018 Jake Warren
+
+[changelog]: https://github.com/jakewarren/jsome/blob/master/CHANGELOG.md
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/cli"
+	"github.com/fatih/color"
+	"github.com/nwidger/jsoncolor"
+	"github.com/spf13/pflag"
+)
+
+func main() {
+
+	displayHelp := pflag.BoolP("help", "h", false, "display help")
+
+	pflag.Parse()
+
+	// override the default usage display
+	if *displayHelp {
+		displayUsage()
+		os.Exit(0)
+	}
+
+	//human-friendly CLI output
+	log.SetHandler(cli.New(os.Stderr))
+
+	//set the logging level
+	log.SetLevel(log.WarnLevel)
+
+	r, err := openStdinOrFile()
+
+	checkError("error acquiring input", err)
+
+
+	prettyPrint(r)
+
+}
+
+func prettyPrint(data io.Reader) {
+	b, _ := ioutil.ReadAll(data)
+
+	// create custom formatter,
+	f := jsoncolor.NewFormatter()
+
+	// set custom colors
+	f.SpaceColor = color.New(color.FgRed, color.Bold)
+	f.CommaColor = color.New(color.FgWhite, color.Bold)
+	f.ColonColor = color.New(color.FgYellow)
+	f.ObjectColor = color.New(color.FgBlue, color.Bold)
+	f.ArrayColor = color.New(color.FgWhite)
+	f.FieldColor = color.New(color.FgGreen)
+	f.StringColor = color.New(color.FgMagenta, color.Bold)
+	f.TrueColor = color.New(color.FgCyan, color.Bold)
+	f.FalseColor = color.New(color.FgHiRed)
+	f.NumberColor = color.New(color.FgHiYellow)
+	f.NullColor = color.New(color.FgWhite, color.Bold)
+	f.StringQuoteColor = color.New(color.FgBlue, color.Bold)
+
+	// colorized output is written to dst
+	dst := &bytes.Buffer{}
+	err := f.Format(dst, []byte(b))
+	checkError("error colorizing JSON",err)
+
+	// print colorized output to stdout
+	fmt.Println(dst.String())
+}
+
+// print custom usage instead of the default provided by pflag
+func displayUsage() {
+
+	fmt.Printf("Usage: jsome [<flags>] [FILE]\n\n")
+	fmt.Printf("Example:\n\tjsome file.json\n")
+	fmt.Printf("\tcat file.json | jsome \n\n")
+	fmt.Printf("Optional flags:\n\n")
+	pflag.PrintDefaults()
+}
+
+// openStdinOrFile reads from stdin or a file based on what input the user provides
+func openStdinOrFile() (io.Reader, error) {
+
+	// try to open a file if exists
+	if len(pflag.Args()) == 1 {
+		r, err := os.Open(pflag.Arg(0))
+		if err != nil {
+			return nil, err
+		}
+		return r, err
+	}
+
+	// no file provided so attempt to read piped data from stdin
+	info, err := os.Stdin.Stat()
+	if err != nil {
+		return nil, err
+	}
+	// ensure stdin is a pipe, bail if not
+	if info.Mode()&os.ModeNamedPipe == 0 {
+		return nil, fmt.Errorf("no input specified")
+	}
+
+	return os.Stdin, err
+}
+
+func checkError(message string,err error) {
+	if err != nil {
+		log.WithError(err).Fatal(message)
+	}
+}
